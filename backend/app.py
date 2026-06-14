cbed82683b41d4ca4bb5cdb5a2cc9a651586ccae
Shaurya <promispriyam04@gmail.com>
2026-06-14 14:58:48 +0530

    UptimeRobot fixed

--- backend/app.py
+++ backend/app.py
@@ -97,7 +97,7 @@ class UserReviewCreate(BaseModel):
 def home():
     return {"message": "Movie Sentiment Analysis API Running"}
 
-@app.get("/health")
+@app.get("/health",methods=["GET", "HEAD"])
 def health():
     return {"status": "healthy"}
 
